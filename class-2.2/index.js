9ff5369f4797a622eaf537c871371035906f8c7b
SaumyaPatil <saupatil10dec@gmail.com>
2025-01-29 14:50:05 +0530

    Creating http server

--- class-2.2/index.js
+++ class-2.2/index.js
@@ -1,4 +1,4 @@
 const express = require("express"); //importing express library written in node package registry
 //difference between fs lib and express lib is that fs lib comes built in with nodejs and we just have to require it but with express, we have to bring it to our system from internet
 //command used to bring express to our machine is npm install express
-
+//else we wouldnt have the access to express code locally
