311e802ecb499ce9174769d0773bc19fcbfe4e28
SaumyaPatil <saupatil10dec@gmail.com>
2025-01-29 14:55:59 +0530

    Creating server using express library

--- class-2.2/index.js
+++ class-2.2/index.js
@@ -1,4 +1,17 @@
-const express = require("express"); //importing express library written in node package registry
+//importing express library written in node package registry
 //difference between fs lib and express lib is that fs lib comes built in with nodejs and we just have to require it but with express, we have to bring it to our system from internet
 //command used to bring express to our machine is npm install express
 //else we wouldnt have the access to express code locally
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {     //This callback function will run when anyone will hit your server.
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
